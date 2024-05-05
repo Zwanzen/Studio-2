a63550af782ce705144a6ffa2e7a3fad2928182f
Zwanzen <47717833+Zwanzen@users.noreply.github.com>
2024-05-05 10:13:43 +0200

    B

--- Assets/Editor/SpreadEditor.cs
+++ Assets/Editor/SpreadEditor.cs
@@ -6,17 +6,68 @@ using UnityEditor;
 [CustomEditor(typeof(SpreadPrefabsAcrossTerrain))]
 public class SpreadEditor : Editor
 {
+    float SminVal = 1;
+    float SminLimit = 0.1f;
+    float SmaxVal = 1;
+    float SmaxLimit = 4;
 
+    float WminVal = 1;
+    float WminLimit = 0.1f;
+    float WmaxVal = 1;
+    float WmaxLimit = 4;
     public override void OnInspectorGUI()
     {
         SpreadPrefabsAcrossTerrain spread = (SpreadPrefabsAcrossTerrain)target;
         spread.prefab = (GameObject)EditorGUILayout.ObjectField("Prefab", spread.prefab, typeof(GameObject), false);
         spread.numClones = EditorGUILayout.IntField("Number of Clones", spread.numClones);
         spread.terrain = (Terrain)EditorGUILayout.ObjectField("Terrain", spread.terrain, typeof(Terrain), true);
+        EditorGUILayout.Space(20);
+
+        EditorGUILayout.LabelField("Group Amount:", spread.Groups.Count.ToString());
+        if(spread.Groups.Count == 0)
+        {
+            EditorGUILayout.LabelField("No Groups");
+        }
+        else
+        {
+            spread.selectedGroup = EditorGUILayout.IntSlider("Selected Group", spread.selectedGroup, 1, spread.Groups.Count);
+        }
+        if (GUILayout.Button("Create Group"))
+        {
+            spread.CreateGroup();
+        }
+        if (GUILayout.Button("Remove Group"))
+        {
+            spread.RemoveGroup();
+        }
+
+        EditorGUILayout.Space(10);
+        if (GUILayout.Button("Fix List"))
+        {
+            spread.FixList();
+        }
+
+        EditorGUILayout.Space(20);
+        spread.randomXZRotation = EditorGUILayout.Toggle("Random XZ Rotation", spread.randomXZRotation);
+        EditorGUILayout.LabelField("Min:", SminVal.ToString());
+        EditorGUILayout.LabelField("Max:", SmaxVal.ToString());
+        EditorGUILayout.MinMaxSlider("Scale Multiplier",ref SminVal, ref SmaxVal, SminLimit, SmaxLimit);
+        spread.randomScaleMultiplierMin = SminVal;
+        spread.randomScaleMultiplierMax = SmaxVal;
+        spread.randomWidth = EditorGUILayout.Toggle("Random Width", spread.randomWidth);
+        if (spread.randomWidth)
+        {
+            EditorGUILayout.LabelField("Min:", WminVal.ToString());
+            EditorGUILayout.LabelField("Max:", WmaxVal.ToString());
+            EditorGUILayout.MinMaxSlider("Scale Multiplier",ref WminVal, ref WmaxVal, WminLimit, WmaxLimit);
+            spread.randomWidthMultiplierMin = WminVal;
+            spread.randomWidthMultiplierMax = WmaxVal;
+        }
 
         if (GUILayout.Button("Spread Prefabs"))
         {
-            spread.CleanChildren();
+            if(spread.Groups.Count != 0)
+                spread.CleanChildren();
             spread.PlaceCopies();
         }
 
--- Assets/Scripts/SpreadPrefabsAcrossTerrain.cs
+++ Assets/Scripts/SpreadPrefabsAcrossTerrain.cs
@@ -1,32 +1,80 @@
 using UnityEngine;
 using UnityEditor;
+using System.Collections.Generic;
 
 [ExecuteInEditMode]
 public class SpreadPrefabsAcrossTerrain : MonoBehaviour
 {
-    // The prefab to be cloned
     public GameObject prefab;
-
-    // The number of clones to spread
     public int numClones;
-
-    // The terrain component
     public Terrain terrain;
 
-    int spawned = 0;
+    public int selectedGroup = 0;
+    public List<Transform> Groups = new List<Transform>();
+
+    public bool randomXZRotation = false;
+    [HideInInspector]
+    public float randomScaleMultiplierMin = 1.0f;
+    [HideInInspector]
+    public float randomScaleMultiplierMax = 1.0f;
+    public bool randomWidth = false;
+    [HideInInspector]
+    public float randomWidthMultiplierMin = 1.0f;
+    [HideInInspector]
+    public float randomWidthMultiplierMax = 1.0f;
 
     public void CleanChildren()
     {
-        int nbChildren = terrain.transform.childCount;
+        int nbChildren = Groups[selectedGroup - 1].transform.childCount;
 
         for (int i = nbChildren - 1; i >= 0; i--)
         {
-            DestroyImmediate(terrain.transform.GetChild(i).gameObject);
+            DestroyImmediate(Groups[selectedGroup - 1].GetChild(i).gameObject);
+        }
+    }
+
+    public void CreateGroup()
+    {
+        GameObject group = new GameObject("Group " + (Groups.Count + 1));
+        group.transform.parent = transform;
+        Groups.Add(group.transform);
+        selectedGroup++;
+    }
+
+    public void RemoveGroup()
+    {
+        // If there are no groups, return
+        if (Groups.Count == 0)
+            return;
+        // If the selected group has transform, destroy it
+        if (Groups[selectedGroup - 1].transform != null)
+            DestroyImmediate(Groups[selectedGroup-1].gameObject);
+
+
+        Groups.RemoveAt(selectedGroup-1);
+        ReorderGroupNames();
+    }
+
+    public void FixList()
+    {
+        Groups.RemoveAt(selectedGroup - 1);
+    }
+
+    private void ReorderGroupNames()
+    {
+        for (int i = 0; i < Groups.Count; i++)
+        {
+            Groups[i].name = "Group " + (i + 1);
         }
     }
 
     public void PlaceCopies()
     {
+        if(Groups.Count == 0)
+        {
+            CreateGroup();
+        }
+
         // Calculate the terrain size
         Vector3 terrainSize = terrain.terrainData.size;
 
@@ -37,29 +85,56 @@ public class SpreadPrefabsAcrossTerrain : MonoBehaviour
         // Calculate the spacing between clones
         float spacing = Mathf.Min(terrainSize.x / clonesPerRow, terrainSize.z / clonesPerCol);
 
-        spawned = 0;
-
         // Spread the clones across the terrain
         for (int i = 0; i < clonesPerRow; i++)
         {
             for (int j = 0; j < clonesPerCol; j++)
             {
+                /*
                 // Calculate the position of the clone
-                float x = i * spacing - (terrainSize.x );
-                float z = j * spacing - (terrainSize.z );
+                float x = i * spacing + terrain.transform.position.x;
+                float z = j * spacing + terrain.transform.position.z;
+                */
+                float tx = terrain.transform.position.x;
+                float tz = terrain.transform.position.z;
+
+
+                Vector3 randPos = new Vector3(Random.Range(tx, terrainSize.x + tx), 0, Random.Range(tz, terrainSize.z + tz));
 
                 // Adding height to the vector
-                Vector3 pos = new Vector3(x, 0, z);
-                pos = new(pos.x, terrain.SampleHeight(pos), pos.z);
+                randPos = new(randPos.x, terrain.SampleHeight(randPos), randPos.z);
+
+                // Rotate to align with terrain normal
+                Vector3 normal = terrain.terrainData.GetInterpolatedNormal((randPos.x - tx) / terrainSize.x, (randPos.z - tz) / terrainSize.z);
+                Quaternion rotation = Quaternion.FromToRotation(Vector3.up, normal);
+
+                // Add a random rotation around local Y
+                rotation *= Quaternion.Euler(0, Random.Range(0, 360), 0);
+                if (randomXZRotation)
+                    rotation *= Quaternion.Euler(Random.Range(0, 360), 0, Random.Range(0, 360));
 
                 // Instantiate the prefab at the calculated position
-                GameObject clone = Instantiate(prefab, pos, Quaternion.identity);
+                GameObject clone = Instantiate(prefab, randPos, Quaternion.identity);
+
+                // Apply rotation
+                clone.transform.rotation = rotation;
+
+                // Apply random scale
+                if (!randomWidth)
+                {
+                    clone.transform.localScale = Vector3.one * Random.Range(randomScaleMultiplierMin, randomScaleMultiplierMax);
+                }
+                else
+                {
+                    float xScale = Random.Range(randomWidthMultiplierMin, randomWidthMultiplierMax);
+                    float yScale = Random.Range(randomScaleMultiplierMin, randomScaleMultiplierMax);
+                    float zScale = Random.Range(randomWidthMultiplierMin, randomWidthMultiplierMax);
+                    clone.transform.localScale = new Vector3(xScale, yScale, zScale);
+                }
 
                 // Set the parent of the clone to the terrain
-                clone.transform.parent = terrain.transform;
-                spawned++;
+                clone.transform.parent = Groups[selectedGroup - 1].transform;
             }
         }
-        Debug.Log(spawned);
     }
 }
